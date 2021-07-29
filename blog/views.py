240e050c8eda6bb44b5450678186eeb6429fdc53
tomokopa <tomokoie1@gmail.com>
2021-07-29 15:10:18 +0900

    210729 delete post-detail

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Post
+#from .models import Post
 # forms関連
 from .forms import ContactForm
 from django.http import HttpResponse
@@ -8,13 +8,8 @@ from django.conf import settings
 from django.core.mail import BadHeaderError, send_mail
 
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
-
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+def top(request):
+    return render(request, 'blog/top.html', {})
 
 def push_timer(request):
     return render(request, 'timer/push_timer.html', {})
